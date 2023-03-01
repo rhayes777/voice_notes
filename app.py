7a8580b600c438023fa49d7a469dd24687262e88
Richard <richardhayes777@gmail.com>
2023-03-01 21:25:10 +0000

    showing summary

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import glob
+import os
+from threading import Thread
 
 from flask import Flask, render_template, request
 import datetime as dt
 import whisper
+import openai
 
 from pathlib import Path
 
@@ -10,7 +13,30 @@ output_directory = Path("output")
 
 app = Flask(__name__)
 
-model = whisper.load_model("base")
+model = whisper.load_model("small")
+
+completion = openai.Completion()
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+
+header = """
+Summarize what is said in the above statements.
+"""
+
+
+def summarize(notes):
+    text = "\n".join(notes)
+    response = completion.create(
+        engine="text-davinci-003",
+        max_tokens=1024,
+        temperature=0.1,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        prompt=text,
+    )
+    return response["choices"][0]["text"]
 
 
 def transcribe(filename):
@@ -19,13 +45,18 @@ def transcribe(filename):
         f.write(new_text)
 
 
+def get_notes():
+    notes = []
+    for filename in glob.glob("output/*.txt"):
+        with open(filename) as f:
+            notes.append(f.read())
+    return notes
+
+
 @app.route('/')
 def index():
-    notes = []
-    for file in sorted(glob.glob(str(output_directory / "*.txt"))):
-        with open(file) as f:
-            notes.append(f.read())
-    return render_template('index.html', notes=notes)
+    notes = get_notes()
+    return render_template('index.html', notes=get_notes(), summary=summarize(notes))
 
 
 @app.route('/upload', methods=['POST'])
@@ -34,7 +65,8 @@ def upload():
     name = dt.datetime.now().isoformat()
     filename = output_directory / f'{name}.wav'
     audio.save(filename)
-    transcribe(filename)
+    thread = Thread(target=transcribe, args=(filename,))
+    thread.start()
     return 'OK'
 
 
