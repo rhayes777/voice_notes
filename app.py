e7c9e8dc49d0495e9476fb2990ab1e85bae37790
Richard <richardhayes777@gmail.com>
2023-03-01 20:47:36 +0000

    front end recording

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask, render_template
-import pyaudio
-import wave
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,42 +8,11 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/record')
-def record():
-    CHUNK = 1024
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 44100
-    RECORD_SECONDS = 5
-    WAVE_OUTPUT_FILENAME = "output.wav"
-
-    p = pyaudio.PyAudio()
-
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
-
-    frames = []
-
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
-
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
-    wf.close()
-
-    return 'Recorded!'
+@app.route('/upload', methods=['POST'])
+def upload():
+    audio = request.files.get('audio')
+    audio.save('audio.wav')
+    return 'OK'
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app.run(debug=True)
