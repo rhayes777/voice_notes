c904a0bc8e4e23ee0a87ee739b95dd586aac0ea0
Richard <richardhayes777@gmail.com>
2023-03-01 21:02:29 +0000

    record and transcribe

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, request
+import datetime as dt
+import whisper
+
+from pathlib import Path
+
+output_directory = Path("output")
 
 app = Flask(__name__)
+
+model = whisper.load_model("base")
+
+
+def transcribe(filename):
+    new_text = model.transcribe(audio=str(filename), language="english")["text"]
+    with open(filename.with_suffix(".txt"), "w") as f:
+        f.write(new_text)
 
 
 @app.route('/')
@@ -11,7 +25,10 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload():
     audio = request.files.get('audio')
-    audio.save('audio.wav')
+    name = dt.datetime.now().isoformat()
+    filename = output_directory / f'{name}.wav'
+    audio.save(filename)
+    transcribe(filename)
     return 'OK'
 
 
